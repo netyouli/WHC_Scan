4b0a799b2ff152876e612ae88be9c883a912604e
netyouli <712641411@qq.com>
2017-01-11 16:47:42 +0800

    Initial Commit

---
+++ WHC_ScanUnusedClass/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WHC_ScanUnusedClass
+//
+//  Created by WHC on 17/1/11.
+//  Copyright © 2017年 WHC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WHC_ScanUnusedClass/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WHC_ScanUnusedClass
+//
+//  Created by WHC on 17/1/11.
+//  Copyright © 2017年 WHC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
